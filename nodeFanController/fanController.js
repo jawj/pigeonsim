fe05e61a7820fc64ea55103c32f4d172c8fad564
Steven Gray <>
2014-06-11 09:12:53 +0100

    Added RGB LED logic and reset fans code to switch off the fans when interface resets.

--- nodeFanController/fanController.js
+++ nodeFanController/fanController.js
@@ -11,9 +11,6 @@ if(boardRun){
 
 var WebSocket = require('ws')
 
-var lowPower = 2;
-var maxPower = 10;
-
 var PWMmapping = {
     "3": 3,
     "5": 5,
@@ -23,17 +20,43 @@ var PWMmapping = {
     "11": 4
 }
 
-var LEDpin = 9;
+var LEDpin = 7;
+
+var LEDPin_R = PWMmapping['5'];
+var LEDPin_G = PWMmapping['6'];
+var LEDPin_B = PWMmapping['9'];
 
 var rightFanPWM = PWMmapping['3'];
-var leftFanPWM = PWMmapping['5'];
+var leftFanPWM = PWMmapping['3'];
+
+var fanSelectLeft = 2;
+var fanSelectRight = 4;
+
+var fanMap = 0; // 1 == Left, 2 = Right, 0 = None
+
 
 if(boardRun){
     board.on("ready", function() {
         var byte = 0;
-        this.pinMode(LEDpin, this.MODES.OUTPUT);
+
+        //Set Fan Output
         this.pinMode(rightFanPWM, this.MODES.OUTPUT);
         this.pinMode(leftFanPWM, this.MODES.OUTPUT);
+
+        // Set Fan Select switches
+        this.pinMode(fanSelectLeft, this.MODES.INPUT);
+        this.pinMode(fanSelectRight, this.MODES.INPUT);
+
+        // Set single LED
+        this.pinMode(LEDpin, this.MODES.OUTPUT);
+
+        //Set RGB LED
+        this.pinMode(LEDPin_R, this.MODES.OUTPUT);
+        this.pinMode(LEDPin_G, this.MODES.OUTPUT);
+        this.pinMode(LEDPin_B, this.MODES.OUTPUT);
+
+        // Board Startup -- Yellow
+        setRGBLED(255,255,0)
 
         wsRegisterEvents(board);
     });
@@ -41,6 +64,22 @@ if(boardRun){
     wsRegisterEvents(undefined);
 }
 
+function setRGBLED(r, g, b){
+    board.analogWrite(LEDPin_R, r);
+    board.analogWrite(LEDPin_G, g);
+    board.analogWrite(LEDPin_B, b);
+}
+
+function flashRGBLed(r, g, b, duration, times){
+    // TODO:  Flash x times
+    setRGBLED(0, 0, 0);
+    setTimeout(function(){
+        setRGBLED(r, g, b);
+        setTimeout(function(){
+            setRGBLED(0, 0, 0);
+        }, duration);
+    }, duration);
+}
 
 function wsRegisterEvents(board){
     //On ready and connected to server!
@@ -49,6 +88,19 @@ function wsRegisterEvents(board){
         console.log("Connected to the FanController");
         if(board != undefined){
             board.digitalWrite(LEDpin, 1);
+            setRGBLED(0, 255, 0); // On Green
+
+            board.digitalRead(fanSelectLeft, function(data) {
+                if(data == 1){
+                    fanMap = 1;
+                }      
+            });
+
+            board.digitalRead(fanSelectRight, function(data) {
+                if(data == 1){
+                    fanMap = 2;
+                }      
+            });
         }
     });
 
@@ -56,31 +108,45 @@ function wsRegisterEvents(board){
         console.log("Server has closed connection");
         if(board != undefined){
             board.digitalWrite(LEDpin, 0);
+            setRGBLED(255, 0, 0); // On Red
         }
     });
 
     ws.on('message', function(message) {
         message = JSON.parse(message);
+        if(message.reset == 1 || message.reset == 2){
+            console.log("RESET: Switching Fans off! \tMode:" + message.reset);
+            if(board != undefined){
+                board.analogWrite(leftFanPWM, 0);
+                board.analogWrite(rightFanPWM, 0);
+            }
+        }
+
         if(message.roll != 0){
-        
             if(message.roll > 0){
                 console.log("Fan Left On: " + message.roll);
                 if(board != undefined){
-                    board.analogWrite(leftFanPWM, 255);
-                    board.analogWrite(rightFanPWM, 0); //Turn off right fan when going right
+                    if(fanMap == 1){
+                        board.analogWrite(leftFanPWM, 255);
+                    }else{
+                        board.analogWrite(rightFanPWM, 0); //Turn off right fan when going right
+                    }
                 }
             }
             
             if(message.roll < 0){
                 console.log("Fan Right On: " + message.roll);
                 if(board != undefined){
-                    board.analogWrite(rightFanPWM, 255);
-                    board.analogWrite(leftFanPWM, 0);  //Turn off left fan when going right
+                    if(fanMap == 2){
+                        board.analogWrite(rightFanPWM, 255);
+                    }else{
+                        board.analogWrite(leftFanPWM, 0);  //Turn off left fan when going right
+                    }
                 }
             }
         
         }else{
-            //console.log("Straight Flight Both Fans On:" + lowPower)
+            console.log("Straight Flight Both Fans On")
             if(board != undefined){
                 board.analogWrite(leftFanPWM, 50);
                 board.analogWrite(rightFanPWM, 50);
